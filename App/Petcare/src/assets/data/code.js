fb5f324f928bb2ab10b3f8597468a12818732a38
Turing1887 <tim.zschage@t-online.de>
2017-06-04 23:07:51 +0200

    update code.js
    * added functionality to amount buttons

--- App/Petcare/src/assets/data/code.js
+++ App/Petcare/src/assets/data/code.js
@@ -1,20 +1,56 @@
-
+let foodClick = 0;
+let waterClick = 0;
 
 $(document).ready(function(){
+
+  // Choose the resource
   $("#food-button").click(function(){
-    message = new Paho.MQTT.Message("4");
-    message.destinationName = "haw/dmi/mt/its/petcare";
-    client.send(message);
-      $(".food-amount").toggleClass("button-active");
-      $(".water-amount").removeClass("button-active");
+    $(".food-amount").toggleClass("button-active");
+    $(".water-amount").removeClass("button-active");
+    foodClick++;
+    if(foodClick%2==1 || waterClick%2==1){
+      $(".scroll-content").animate({ scrollTop: $(document).height() }, "slow");
+    }
   });
   $("#water-button").click(function(){
-    message = new Paho.MQTT.Message("1");
+    waterClick++;
+    if(foodClick%2==1 || waterClick%2==1){
+      $(".scroll-content").animate({ scrollTop: $(document).height() }, 1000);
+    }
+    $(".water-amount").toggleClass("button-active");
+    $(".food-amount").removeClass("button-active");
+  });
+
+
+  //Amount of food to be deployed
+  $("#smallFood-btn").click(function(){
+    message = new Paho.MQTT.Message("00");
     message.destinationName = "haw/dmi/mt/its/petcare";
     client.send(message);
-      $(".water-amount").toggleClass("button-active");
-      $(".food-amount").removeClass("button-active");
+    $(".food-amount").toggleClass("button-active");
   });
+  $("#bigFood-btn").click(function(){
+    message = new Paho.MQTT.Message("01");
+    message.destinationName = "haw/dmi/mt/its/petcare";
+    client.send(message);
+    $(".food-amount").toggleClass("button-active");
+  });
+
+  //Amount of water to be deployed
+  $("#smallWater-btn").click(function(){
+    message = new Paho.MQTT.Message("10");
+    message.destinationName = "haw/dmi/mt/its/petcare";
+    client.send(message);
+    $(".water-amount").toggleClass("button-active");
+  });
+  $("#bigWater-btn").click(function(){
+    message = new Paho.MQTT.Message("11");
+    message.destinationName = "haw/dmi/mt/its/petcare";
+    client.send(message);
+    $(".water-amount").toggleClass("button-active");
+  });
+
+
 });
 
 // Create a client instance
